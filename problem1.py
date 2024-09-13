1016d9b3c9b5e7634d2a9fee5b0d567cd8df626c
Gogineni Pallavi Chowdary <86236414+goginenipallavichowdary@users.noreply.github.com>
2024-09-13 15:05:41 -0500

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -24,3 +24,10 @@ for array in input_arrays:
     combined_array.extend(array)
 
 print("Merged array in a sorted order:", insertion_sort(combined_array))
+
+
+'''Input: K = 3, N =  4
+array1 = [1,3,5,7]
+array2 = [2,4,6,8]
+array3 = [0,9,10,11]
+Output: [0,1,2,3,4,5,6,7,8,9,10,11]'''
